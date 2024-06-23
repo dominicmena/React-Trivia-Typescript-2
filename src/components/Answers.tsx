e8dc04d0933398a057505b8c4b9587d62cc6ab39
dominicmena <mena.dominic@gmail.com>
2024-06-23 02:07:41 -0500

    created usetate for showanswer, setshowanswer. made function for onpress where it's accepting an idx number, changes the state to true, and runs the onSubmit function where the correctanswer idx is equal to the index passed. and logic for color of text if answer is correct or incorrect

---
+++ src/components/Answers.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Question } from "../types";
+import Answer from "./Answer";
+
+type Props = {
+    question: Question;
+    onSubmit: (correct: boolean) => void;
+};
+
+function Answers(props: Props) {
+const [showAnswer, setShowAnswer] = useState(false);
+
+const onPress = (idx: number) => {
+    setShowAnswer(true)
+    props.onSubmit(props.question.correctAnswerIdx === idx)
+}
+
+    return (
+    <div> 
+        {props.question.choices.map((choice, idx) => {
+            let color = ""
+
+            if (showAnswer && props.question.correctAnswerIdx === idx) color = 'green';
+            else if (showAnswer) color = 'red'
+            return <Answer text={choice} onPress={() => onPress(idx)} color={color}/>
+})}
+    </div>
+    );
+}
+
+export default Answers
