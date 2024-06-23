c8626811f3d20173baf09fc3792bf405c2708b13
dominicmena <mena.dominic@gmail.com>
2024-06-23 01:23:55 -0500

    created components folder and added statbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import styles from './App.module.scss';
 import questions from './questions.json'
+import { Questions } from './types';
+import { useState } from 'react';
 
 function App() {
+    const allQuestions = questions as Questions
+
+    const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0)
+    const [correctAnswers, setCorrectAnswers] = useState(0)
+    const [incorrectAnswers, setIncorrectAnswers] = useState(0)
+    
     return (
         <div>
      
